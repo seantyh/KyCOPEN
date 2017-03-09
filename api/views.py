7fd0e20b6a5c632619a789be92e8cf41b58b171a
root <seantyh@gmail.com>
2017-03-09 15:23:42 +0000

    Fix arguments in decorators

--- api/views.py
+++ api/views.py
@@ -14,25 +14,27 @@ import time
 
 def ip_throttle(ip_args):    
     def deco_func(func):
-	def wrapper(*args, **kw):
-	    try:
+        def wrapper(*args, **kw):
+            try:
                 ip_buckets = ip_args[0]
-		req = args[0]
-		if len(ip_buckets) > 10:
-		    ip_buckets = ip_buckets[1:]
-		req_ip = req.META.get("REMOTE_ADDR")
-		last_req_t = ip_buckets.get(req_ip, 0)
-		if time.time() - last_req_t < 5:                
-		    ip_buckets[req_ip] = time.time()
-		    return HttpResponse(jsonDump({"status": "too frequent"}), status = 429)
-		else:
-		    return func(*args, **kw)
-	    except Exception as ex:
-		return HttpResponse(jsonDump({"status": "rate limit exception", "msg": str(ex)}), status = 500)
-	return wrapper
+                req = args[0]
+                if len(ip_buckets) > 10:
+                    keys = sorted(ip_buckets.key(), key = ip_buckets.get)
+                    del ip_buckets[keys[0]]
+                req_ip = req.META.get("REMOTE_ADDR")
+                print(req_ip)
+                last_req_t = ip_buckets.get(req_ip, 0)
+                if time.time() - last_req_t < 5:                
+                    ip_buckets[req_ip] = time.time()
+                    return HttpResponse(jsonDump({"status": "too frequent"}), status = 429)
+                else:
+                    return func(*args, **kw)
+            except Exception as ex:
+                return HttpResponse(jsonDump({"status": "rate limit exception", "msg": str(ex)}), status = 500)
+        return wrapper
     return deco_func
 
-ip_records = []
+ip_records = {}
 def jsonDump(dic):
     return json.dumps(dic, ensure_ascii=False, indent=4)
     
@@ -75,8 +77,8 @@ from django.core.paginator import Paginator
 
 @ip_throttle([ip_records])
 def thesaurus(request, word):    
-    res = getThesaurus(word)
-
+    # res = getThesaurus(word)
+    res = {"err": "skipped"}
 #    paginator = Paginator(res, 2)
 ##    page = paginator.page(1)
 #    page = paginator.page(int(request.GET.get('page', '1')))
